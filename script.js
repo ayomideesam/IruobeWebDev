26d052bda5226e086841d18405d51458b55b239e
Iruobe Akhigbe <iruobeakhigbe@gmail.com>
2019-04-09 16:40:47 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/*
+alert("What is your name?");
+
+[1,2].forEach(alert)
+
+
+(function() {
+'use strict';
+})()
+
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+\" double quote
+
+
+Angular Basics
+
+*** node -v
+*** npm install -g @angular/cli
+*** ng --v
+
+*/
+
+var a;
+a = "Iruobe Development";
+
+console.log(a)
+
+var a = 5;
+var b = 10
+var c = "I am a string";
+
+a += 2;
+b -= 3;
+c = c + " variable!";
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+var sum = 25 * 25;
+console.log(sum)
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+var firstName = "Alan";
+var lastName = "Turing";
+
+var myFirstName = "Beau";
+var myLastName = " Carnes";
+console.log(myFirstName + myLastName)
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr)
+
+var firstName = "Alan. ";
+firstName += "Turing";
+console.log(firstName)
